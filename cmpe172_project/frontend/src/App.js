43d11fced5e367aa9d48d5e642dfd3784efc73e4
Hibo <hibozahara@gmail.com>
2020-11-11 14:03:29 -0800

    remove empty directory

--- cmpe172_project/frontend/src/App.js
+++ cmpe172_project/frontend/src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
-import AddCustomer from "./Component/AddCustomer"
+
 import AddProduct from "./Component/AddProduct";
-import AddSale from "./Component/AddSale";
+
 import { Route, BrowserRouter as Router } from "react-router-dom";
-import Customer from "./Component/Customer";
+
 import Product from "./Component/Product";
-import Sale from "./Component/Sale";
 import Sidebar from "./Component/NavBar"
 import Home from "./Component/Home"
-import Analytics from "./Component/Analytics"
+
 class App extends Component {
   
   render() {
@@ -21,8 +20,6 @@ class App extends Component {
         <Route exact path="/AddProduct" component={AddProduct} />
         
         <Route exact path="/product" component={Product} />
-        
-        <Route exact path="/Analytics" component={Analytics} />
         <Route exact path="/" component={Home} />
       </Router>
       </div>
