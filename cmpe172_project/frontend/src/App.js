e05f5d7e1fabc78eea8b4e08038fd4d2fdc032d6
gurvinskohli <54507769+gurvinskohli@users.noreply.github.com>
2020-12-06 22:09:46 -0800

    About and contact pages

--- cmpe172_project/frontend/src/App.js
+++ cmpe172_project/frontend/src/App.js
@@ -8,17 +8,21 @@ import Recipe from "./Component/Recipe";
 import Sidebar from "./Component/NavBar";
 import Home from "./Component/Home";
 import Share from "./Component/share";
-
-
+import suggestions from "./Component/suggestions";
+import AboutUs from "./Component/AboutUs";
+import ContactUs from "./Component/ContactUs";
 
 class App extends Component {
-  
+
   render() {
     return (
       <div id="App">
       <Sidebar/>
       <Router>
         <Route exact path="/Home" component={Home} />
+	       <Route exact path="/suggestions" component={suggestions} />
+         <Route exact path="/AboutUs" component={AboutUs} />
+         <Route exact path="/ContactUs" component={ContactUs} />
         <Route exact path="/AddRecipe" component={AddRecipe} />
         <Route exact path="/recipe" component={Recipe} />
 	<Route exact path="/share" component={Share} />
@@ -26,7 +30,7 @@ class App extends Component {
 
       </Router>
       </div>
-      
+
     );
   }
 }
